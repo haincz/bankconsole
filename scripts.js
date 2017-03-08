ce6b9cb8dd84f34d4972d47ee80387510f279c03
Mateusz Mikoś <Mateusz Miko>
2017-03-08 19:51:26 +0100

    class constructor

--- scripts.js
+++ scripts.js
@@ -1,25 +1,50 @@
+var correctPin = 4567;
+
+
+function Pinpad() {
+  this.pin = [];
+}
+
+Pinpad.prototype.push = function(buttonPressed) {
+  this.pin.push(buttonPressed);
+}
+
+Pinpad.prototype.get = function() {
+  return this.pin.join('');
+}
+
+var pinpad = new Pinpad();
+
 $(document).ready(function(){
 
-  $('.btn-default').on('click', function(event) {
-    var buttonPressed = $(this).text();
-    console.log(buttonPressed);
+	$('.btn-default').on('click', function(event) {
+ 		var buttonPressed = $(this).text();
+  
+		pinpad.push(buttonPressed);
+  		var pinData = pinpad.get();
 
-    drawPin();
-   });
+  	
+  		drawPin();
+   	});
 
-function drawPin() {
-  var result = "*";
-  $('#pin').append(result);
-}
+   	function drawPin() {
+  		var result = "*";
+  		$('#pin').append(result);
+	}
 
-$('#c').on('click', function(event){
-  resetDrawPin();
+	$('#c').on('click', function(event){
+  		resetDrawPin();
 
-})
+	})
 
-function resetDrawPin() {
-  $('#pin').text("");
-}
+	function resetDrawPin() {
+  		$('#pin').text("");
+	}
+
+	$('#ok').on('click', function(event){
+		checkPin();
+	})
 
 
+	
 });
